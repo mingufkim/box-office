0d8e60af2356bdef1d25470746e68e7f97d13eb8
Mingu Kim <mingufkim@gmail.com>
2024-03-07 18:35:00 +0900

    ✨ 날짜

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import "./App.css"
+import './App.css'
 
 function App() {
+  let [year, month, yesterday] = new Date(Date.now() - 86400000)
+    .toISOString()
+    .split('T')[0]
+    .split('-')
+
   return (
     <>
       <h1>박스오피스</h1>
+      <h2>
+        {year}. {month}. {yesterday}.
+      </h2>
     </>
   )
 }
