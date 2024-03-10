df1dadfa41122655f63bc612745c62e020880526
Mingu Kim <mingufkim@gmail.com>
2024-03-10 16:08:50 +0900

    ✨ 일별 박스오피스

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
@@ -6,12 +7,52 @@ function App() {
     .split('T')[0]
     .split('-')
 
+  const [loading, setLoading] = useState(true)
+
+  const [dailyBoxOfficeList, setDailyBoxOfficeList] = useState([])
+
+  const fetchDailyBoxOfficeList = async () => {
+    const response = await fetch(
+      `http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=${
+        import.meta.env.VITE_KOFIC_API_KEY
+      }&targetDt=${year}${month}${yesterday}`
+    )
+    const data = await response.json()
+    setDailyBoxOfficeList(data.boxOfficeResult.dailyBoxOfficeList)
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    fetchDailyBoxOfficeList()
+  }, [])
+
   return (
     <>
       <h1>박스오피스</h1>
       <h2>
         {year}. {month}. {yesterday}.
       </h2>
+      {loading ? (
+        <h3>불러오는 중</h3>
+      ) : (
+        <ol>
+          {dailyBoxOfficeList.map((movie) => (
+            <li key={movie.rank}>
+              <h3>
+                {movie.rank}{' '}
+                {movie.rankInten === '0'
+                  ? ''
+                  : movie.rankInten > 0
+                  ? '⬆️'
+                  : '⬇️'}
+              </h3>
+              <h3>
+                {movie.rankOldAndNew === 'OLD' ? '' : '🆕'} {movie.movieNm}
+              </h3>
+            </li>
+          ))}
+        </ol>
+      )}
     </>
   )
 }
